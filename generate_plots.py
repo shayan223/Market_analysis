673247544e947bcb3487df7f99d80376848658c2
shayan223 <shayan2@pdx.edu>
2022-01-21 14:19:15 -0800

    generalized plot generator
    plot generation can now be used to create different time resolutions

--- generate_plots.py
+++ generate_plots.py
@@ -5,22 +5,23 @@ from tqdm import tqdm
 import os
 
 
-def gen_day_candlestick(root, time_window):
+
+def gen_candlestick(root, time_window, time_scale,data_file):
 
     #Create requisite directory structure
-    outdir = root+'/daily/candle_stick'
+    outdir = root+'/'+time_scale+'/candle_stick'
     if not os.path.exists(outdir):
         os.makedirs(outdir,exist_ok=True)
 
 
-    #Load day-by-day resolution data
+    #Load 'time_scale' resolution data
     #Volume is by USD and not ETH (ETH volume is dropped)
     #Args are used to specifically read dat column as a DateTimeIndex
-    day_data = pd.read_csv(root+'/ETH_day.csv',index_col=0,parse_dates=True)
-    day_data = day_data.drop(['Symbol', 'Volume ETH'], axis=1)
-    day_data = day_data.rename(columns={'Volume USD':'Volume'})
-    day_data = day_data.iloc[::-1]
-    print(day_data.head())
+    data = pd.read_csv(root+'/'+data_file,index_col=0,parse_dates=True)
+    data = data.drop(['Symbol', 'Volume ETH'], axis=1)
+    data = data.rename(columns={'Volume USD':'Volume'})
+    data = data.iloc[::-1]
+    print(data.head())
 
     #Label each image based on closing price increase/drop for the given time step
     price_change_labels = []
@@ -36,15 +37,17 @@ def gen_day_candlestick(root, time_window):
 
     #Plot day-by-day candle-stick chart, with previous 'time_window' days history, for every day
     #We do not include the very last day because we do not know its price change in the following time step.
-    for i in tqdm(range(day_data.shape[0]-time_window-1)):
-        current_timeStep = day_data.iloc[i:i+time_window]
-        mpf.plot(current_timeStep, type='candle', volume=True, style='yahoo', savefig=root+'/daily/candle_stick/timestep_'+str(i))
+    for i in tqdm(range(data.shape[0]-time_window-1)):
+        current_timeStep = data.iloc[i:i+time_window]
+        # Change size of saved figures to match resnet image resolution of 224x224 (default dpi of 100 used in calculation below)
+        mpf.plot(current_timeStep, type='candle', volume=True, style='yahoo', savefig=root+'/'+time_scale+'/candle_stick/timestep_'+str(i),figsize=(224/100,224/100))
+
 
         # Initialise t+1 price for label generation
-        resulting_price = day_data.iloc[i+(time_window + 1)]['Close']
+        resulting_price = data.iloc[i+(time_window + 1)]['Close']
 
         #The label will be the net change in price at closing time from the current time step to the next
-        cur_price = day_data.iloc[i+time_window]['Close']
+        cur_price = data.iloc[i+time_window]['Close']
         price_change = resulting_price - cur_price
         price_change_labels.append(price_change)
         percent_change_labels.append(price_change / cur_price)
@@ -62,15 +65,15 @@ def gen_day_candlestick(root, time_window):
     labels['resulting_price'] = result_price_labels
     labels['file_name'] = file_name_labels
 
-    labels.to_csv(root+'/daily/candle_stick/labels.csv')
+    labels.to_csv(root+'/'+time_scale+'/candle_stick/labels.csv')
 
 
 
 
-def gen_day_priceLine(root, time_window):
+def gen_priceLine(root, time_window,time_scale,data_file):
 
     #Create requisite directory structure
-    outdir = root+'/daily/price_line'
+    outdir = root+'/'+time_scale+'/price_line'
     if not os.path.exists(outdir):
         os.makedirs(outdir,exist_ok=True)
 
@@ -78,11 +81,11 @@ def gen_day_priceLine(root, time_window):
     #Load day-by-day resolution data
     #Volume is by USD and not ETH (ETH volume is dropped)
     #Args are used to specifically read dat column as a DateTimeIndex
-    day_data = pd.read_csv(root+'/ETH_day.csv',index_col=0,parse_dates=True)
-    day_data = day_data.drop(['Symbol', 'Volume ETH'], axis=1)
-    day_data = day_data.rename(columns={'Volume USD':'Volume'})
-    day_data = day_data.iloc[::-1]
-    print(day_data.head())
+    data = pd.read_csv(root+'/'+data_file,index_col=0,parse_dates=True)
+    data = data.drop(['Symbol', 'Volume ETH'], axis=1)
+    data = data.rename(columns={'Volume USD':'Volume'})
+    data = data.iloc[::-1]
+    print(data.head())
 
     #Label each image based on closing price increase/drop for the given time step
     price_change_labels = []
@@ -98,15 +101,16 @@ def gen_day_priceLine(root, time_window):
 
     #Plot day-by-day candle-stick chart, with previous 'time_window' days history, for every day
     #We do not include the very last day because we do not know its price change in the following time step.
-    for i in tqdm(range(day_data.shape[0]-time_window-1)):
-        current_timeStep = day_data.iloc[i:i+time_window]
-        mpf.plot(current_timeStep, type='line', volume=True, style='yahoo', savefig=root+'/daily/price_line/timestep_'+str(i))
+    for i in tqdm(range(data.shape[0]-time_window-1)):
+        current_timeStep = data.iloc[i:i+time_window]
+        # Change size of saved figures to match resnet image resolution of 224x224 (default dpi of 100 used in calculation below)
+        mpf.plot(current_timeStep, type='line', volume=True, style='yahoo', savefig=root+'/'+time_scale+'/price_line/timestep_'+str(i),figsize=(224/100,224/100))
 
         # Initialise t+1 price for label generation
-        resulting_price = day_data.iloc[i+(time_window + 1)]['Close']
+        resulting_price = data.iloc[i+(time_window + 1)]['Close']
 
         #The label will be the net change in price at closing time from the current time step to the next
-        cur_price = day_data.iloc[i+time_window]['Close']
+        cur_price = data.iloc[i+time_window]['Close']
         price_change = resulting_price - cur_price
         price_change_labels.append(price_change)
         percent_change_labels.append(price_change / cur_price)
@@ -124,14 +128,14 @@ def gen_day_priceLine(root, time_window):
     labels['resulting_price'] = result_price_labels
     labels['file_name'] = file_name_labels
 
-    labels.to_csv(root+'/daily/price_line/labels.csv')
+    labels.to_csv(root+'/'+time_scale+'/price_line/labels.csv')
 
 
 
-def gen_day_PandF(root, time_window):
+def gen_PandF(root, time_window,time_scale,data_file):
 
     #Create requisite directory structure
-    outdir = root+'/daily/PandF'
+    outdir = root+'/'+time_scale+'/PandF'
     if not os.path.exists(outdir):
         os.makedirs(outdir,exist_ok=True)
 
@@ -139,11 +143,11 @@ def gen_day_PandF(root, time_window):
     #Load day-by-day resolution data
     #Volume is by USD and not ETH (ETH volume is dropped)
     #Args are used to specifically read dat column as a DateTimeIndex
-    day_data = pd.read_csv(root+'/ETH_day.csv',index_col=0,parse_dates=True)
-    day_data = day_data.drop(['Symbol', 'Volume ETH'], axis=1)
-    day_data = day_data.rename(columns={'Volume USD':'Volume'})
-    day_data = day_data.iloc[::-1]
-    print(day_data.head())
+    data = pd.read_csv(root+'/'+data_file,index_col=0,parse_dates=True)
+    data = data.drop(['Symbol', 'Volume ETH'], axis=1)
+    data = data.rename(columns={'Volume USD':'Volume'})
+    data = data.iloc[::-1]
+    print(data.head())
 
     #Label each image based on closing price increase/drop for the given time step
     price_change_labels = []
@@ -159,15 +163,16 @@ def gen_day_PandF(root, time_window):
 
     #Plot day-by-day point and figure chart, with previous 'time_window' days history, for every day
     #We do not include the very last day because we do not know its price change in the following time step.
-    for i in tqdm(range(day_data.shape[0]-time_window-1)):
-        current_timeStep = day_data.iloc[i:i+time_window]
-        mpf.plot(current_timeStep, type='pnf', style='yahoo', savefig=root+'/daily/PandF/timestep_'+str(i))
+    for i in tqdm(range(data.shape[0]-time_window-1)):
+        current_timeStep = data.iloc[i:i+time_window]
+        # Change size of saved figures to match resnet image resolution of 224x224 (default dpi of 100 used in calculation below)
+        mpf.plot(current_timeStep, type='pnf', style='yahoo', savefig=root+'/'+time_scale+'/PandF/timestep_'+str(i),figsize=(224/100,224/100))
 
         # Initialise t+1 price for label generation
-        resulting_price = day_data.iloc[i+(time_window + 1)]['Close']
+        resulting_price = data.iloc[i+(time_window + 1)]['Close']
 
         #The label will be the net change in price at closing time from the current time step to the next
-        cur_price = day_data.iloc[i+time_window]['Close']
+        cur_price = data.iloc[i+time_window]['Close']
         price_change = resulting_price - cur_price
         price_change_labels.append(price_change)
         percent_change_labels.append(price_change / cur_price)
@@ -185,13 +190,13 @@ def gen_day_PandF(root, time_window):
     labels['resulting_price'] = result_price_labels
     labels['file_name'] = file_name_labels
 
-    labels.to_csv(root+'/daily/PandF/labels.csv')
+    labels.to_csv(root+'/'+time_scale+'/PandF/labels.csv')
 
 
-def gen_day_renko(root, time_window):
+def gen_renko(root, time_window,time_scale,data_file):
 
     #Create requisite directory structure
-    outdir = root+'/daily/renko'
+    outdir = root+'/'+time_scale+'/renko'
     if not os.path.exists(outdir):
         os.makedirs(outdir,exist_ok=True)
 
@@ -199,11 +204,11 @@ def gen_day_renko(root, time_window):
     #Load day-by-day resolution data
     #Volume is by USD and not ETH (ETH volume is dropped)
     #Args are used to specifically read dat column as a DateTimeIndex
-    day_data = pd.read_csv(root+'/ETH_day.csv',index_col=0,parse_dates=True)
-    day_data = day_data.drop(['Symbol', 'Volume ETH'], axis=1)
-    day_data = day_data.rename(columns={'Volume USD':'Volume'})
-    day_data = day_data.iloc[::-1]
-    print(day_data.head())
+    data = pd.read_csv(root+'/'+data_file,index_col=0,parse_dates=True)
+    data = data.drop(['Symbol', 'Volume ETH'], axis=1)
+    data = data.rename(columns={'Volume USD':'Volume'})
+    data = data.iloc[::-1]
+    print(data.head())
 
     # Label each image based on closing price increase/drop for the given time step
     price_change_labels = []
@@ -219,15 +224,16 @@ def gen_day_renko(root, time_window):
 
     #Plot day-by-day renko chart, with previous 'time_window' days history, for every day
     #We do not include the very last day because we do not know its price change in the following time step.
-    for i in tqdm(range(day_data.shape[0]-time_window-1)):
-        current_timeStep = day_data.iloc[i:i+time_window]
-        mpf.plot(current_timeStep, type='renko', style='yahoo', savefig=root+'/daily/renko/timestep_'+str(i))
+    for i in tqdm(range(data.shape[0]-time_window-1)):
+        current_timeStep = data.iloc[i:i+time_window]
+        # Change size of saved figures to match resnet image resolution of 224x224 (default dpi of 100 used in calculation below)
+        mpf.plot(current_timeStep, type='renko', style='yahoo', savefig=root+'/'+time_scale+'/renko/timestep_'+str(i),figsize=(224/100,224/100))
 
         # Initialise t+1 price for label generation
-        resulting_price = day_data.iloc[i+(time_window + 1)]['Close']
+        resulting_price = data.iloc[i+(time_window + 1)]['Close']
 
         #The label will be the net change in price at closing time from the current time step to the next
-        cur_price = day_data.iloc[i+time_window]['Close']
+        cur_price = data.iloc[i+time_window]['Close']
         price_change = resulting_price - cur_price
         price_change_labels.append(price_change)
         percent_change_labels.append(price_change / cur_price)
@@ -244,15 +250,15 @@ def gen_day_renko(root, time_window):
     labels['resulting_price'] = result_price_labels
     labels['file_name'] = file_name_labels
 
-    labels.to_csv(root+'/daily/renko/labels.csv')
+    labels.to_csv(root+'/'+time_scale+'/renko/labels.csv')
 
 
 
 
-def gen_day_movingAvg(root, time_window):
+def gen_movingAvg(root, time_window,time_scale,data_file):
 
     #Create requisite directory structure
-    outdir = root+'/daily/movingAvg'
+    outdir = root+'/'+time_scale+'/movingAvg'
     if not os.path.exists(outdir):
         os.makedirs(outdir,exist_ok=True)
 
@@ -260,11 +266,11 @@ def gen_day_movingAvg(root, time_window):
     #Load day-by-day resolution data
     #Volume is by USD and not ETH (ETH volume is dropped)
     #Args are used to specifically read dat column as a DateTimeIndex
-    day_data = pd.read_csv(root+'/ETH_day.csv',index_col=0,parse_dates=True)
-    day_data = day_data.drop(['Symbol', 'Volume ETH'], axis=1)
-    day_data = day_data.rename(columns={'Volume USD':'Volume'})
-    day_data = day_data.iloc[::-1]
-    print(day_data.head())
+    data = pd.read_csv(root+'/'+data_file,index_col=0,parse_dates=True)
+    data = data.drop(['Symbol', 'Volume ETH'], axis=1)
+    data = data.rename(columns={'Volume USD':'Volume'})
+    data = data.iloc[::-1]
+    print(data.head())
 
     #Label each image based on closing price increase/drop for the given time step
     price_change_labels = []
@@ -279,21 +285,21 @@ def gen_day_movingAvg(root, time_window):
 
     #Plot day-by-day candle-stick chart, with previous 'time_window' days history, for every day
     #We do not include the very last day because we do not know its price change in the following time step.
-    for i in tqdm(range(day_data.shape[0]-time_window-1)):
-        current_timeStep = day_data.iloc[i:i+time_window]
+    for i in tqdm(range(data.shape[0]-time_window-1)):
+        current_timeStep = data.iloc[i:i+time_window]
 
         #For moving averages, we can use aproximately 3/4, 1/2, and 1/4 of the chosen time period
         mav1 = int(np.floor(.75 * time_window))
         mav2 = int(np.floor(.5 * time_window))
         mav3 = int(np.floor(.25 * time_window))
-
-        mpf.plot(current_timeStep, type='candle', mav=(mav1,mav2,mav3), savefig=root+'/daily/movingAvg/timestep_'+str(i))
+        # Change size of saved figures to match resnet image resolution of 224x224 (default dpi of 100 used in calculation below)
+        mpf.plot(current_timeStep, type='candle', mav=(mav1,mav2,mav3), savefig=root+'/'+time_scale+'/movingAvg/timestep_'+str(i),figsize=(224/100,224/100))
 
         # Initialise t+1 price for label generation
-        resulting_price = day_data.iloc[i+(time_window + 1)]['Close']
+        resulting_price = data.iloc[i+(time_window + 1)]['Close']
 
         #The label will be the net change in price at closing time from the current time step to the next
-        cur_price = day_data.iloc[i+time_window]['Close']
+        cur_price = data.iloc[i+time_window]['Close']
         price_change = resulting_price - cur_price
         price_change_labels.append(price_change)
         percent_change_labels.append(price_change/cur_price)
@@ -311,17 +317,26 @@ def gen_day_movingAvg(root, time_window):
     labels['resulting_price'] = result_price_labels
     labels['file_name'] = file_name_labels
 
-    labels.to_csv(root+'/daily/movingAvg/labels.csv')
+    labels.to_csv(root+'/'+time_scale+'/movingAvg/labels.csv')
 
 '''###############################################'''
+
 root = './data'
 time_window=30
-gen_day_candlestick(root,time_window)
-gen_day_priceLine(root,time_window)
-gen_day_PandF(root,time_window)
-gen_day_renko(root,time_window)
-gen_day_movingAvg(root,time_window)
 
+gen_candlestick(root,time_window,'daily','ETH_day.csv')
+gen_priceLine(root,time_window,'daily','ETH_day.csv')
+gen_PandF(root,time_window,'daily','ETH_day.csv')
+gen_renko(root,time_window,'daily','ETH_day.csv')
+gen_movingAvg(root,time_window,'daily','ETH_day.csv')
+
+'''
+gen_candlestick(root,time_window,'daily')
+gen_priceLine(root,time_window,'daily')
+gen_PandF(root,time_window,'daily')
+gen_renko(root,time_window,'daily')
+gen_movingAvg(root,time_window,'daily')
+'''
 
 
 
