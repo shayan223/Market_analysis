55a35f934c4b84b3aefea18cdeaad7be4ed9638a
shayan223 <shayan2@pdx.edu>
2022-03-04 16:49:34 -0800

    implemented LSTM based model
    made a baseline CNN-LSTM model as well as begun work on the DEEP-Q application of the CNN-LSTM

--- cnn_lstm_deep_q.py
+++ cnn_lstm_deep_q.py
@@ -47,11 +47,11 @@ EPS_START = 0.3
 EPS_END = 0.05
 EPS_DECAY = 200
 TARGET_UPDATE = 25
-NUM_EPISODES = 2000
-STEPS_PER_EP = 24
+NUM_EPISODES = 2
+STEPS_PER_EP = 4
 DATA_ROOT = './data/hourly/'
 VALIDATION_SPLIT = .2
-VALIDATION_EPISODES = 500
+VALIDATION_EPISODES = 2
 
 
 class environment:
@@ -99,7 +99,35 @@ class environment:
         print("Training: t=0 to t=",self.max_steps,"Validation: t=",self.validation_index," to t=",self.total_steps)
 
 
-    def step(self):
+    def step(self, action):
+
+        #returns reward and binary for whether the last step has been reached
+
+        #reward nothing if price doesn't change
+        reward = 0
+
+        price_change_percent = self.current_label
+        print(self.current_label)
+        #our reward function: (close - open)/open (this is handled implicitly in the label
+
+        #reward buying before price increases
+        #Punish when buying before price decreases
+        # TODO Current assumption: 0 sell, 1 buy, 2 do nothing
+        if price_change_percent > 0 and action == 1:
+            reward = 1
+        if price_change_percent < 0 and action == 1:
+            reward = -1
+
+        #reward for selling before price decreases
+        #punishing for selling before price increases
+        if price_change_percent < 0 and action == 0:
+            reward = 1
+        if price_change_percent > 0 and action == 0:
+            reward = -1
+
+        #No loss in points when a "no-action" is taken, this cannot be taken during training
+        if action == 2:
+            reward = 0
 
         done = 0
 
@@ -117,7 +145,7 @@ class environment:
             done = 1
 
 
-        return done
+        return reward, done
 
     def get_state_internal(self, time_step):
 
@@ -152,6 +180,13 @@ class environment:
         return self.get_state_internal(self.cur_time_step)
 
 
+'''
+Based on the following tutorial 
+https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+'''
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
 #LSTM code based on the following tutorial https://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
 # as well as this project combining CNN's and LSTM:
@@ -185,7 +220,7 @@ class Ensemble(nn.Module):
         hidden = None
         for t in range(sequence.size(1)):
             xlist = sequence[:,t,:,:,:,:]#list of 3-dim images at given time step t
-
+            print(xlist.shape)
             x1 = xlist[:,0].to(device)
             x2 = xlist[:,1].to(device)
             x3 = xlist[:,2].to(device)
@@ -200,6 +235,7 @@ class Ensemble(nn.Module):
 
             x = torch.cat((x1,x2,x3,x4,x5),dim=1)
 
+            print(x.shape)
             out, hidden = self.lstm(x.unsqueeze(0), hidden)
 
         x = self.fc1(out[-1, :, :])
@@ -218,6 +254,22 @@ def load_model(encoding_dim,model_path=None):
 
     return model
 
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([],maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
 
 
 
@@ -245,23 +297,130 @@ target_net = Ensemble(cnn1, cnn2, cnn3, cnn4, cnn5,num_classes=n_actions).to(dev
 target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
-optimizer = torch.optim.Adam(policy_net.parameters(), lr=.01)
-criterion = nn.CrossEntropyLoss()
+optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(REPLAY_MEMORY)
+
+
 steps_done = 0
 
 
+def select_action(state,confidence_threshold=None):
+    global steps_done
+    sample = random.random()
+    #update epsilon (greedy) values
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    #Take normal action with chance 1-e, random action with chance e
+    if sample > eps_threshold:
+        '''
+        with torch.no_grad():
+            # t.max(1) will return largest column value of each row.
+            # second column on max result is index of where max element was
+            # found, so we pick action with the larger expected reward.
+            return policy_net(state).max(1)[1].view(1, 1)'''
+        policy_net.eval()
 
-episode_accuracy = []
+        val = policy_net(state)
+        val = val.max(1)[1].view(1, 1)
+        #TODO: Implement the following for validation only
+        '''
+        #Confidence between sell and buy (difference in probabilities)
+        confidence = torch.abs((val[0][0] - val[0][1]))
+
+        #if the confidence is worse than the threshold, hold rather than attempt to sell or buy
+        if(confidence < confidence_threshold):
+            val = torch.tensor([[2]], device=device, dtype=torch.long)
 
 
-#TODO Currently many-to-one prediction, consider training by predicting many-to-many
-#TODO do this by returning all hidden states as sequential predictions and comparing to labels for every sequence step
+        #otherwise take the most confident action
+        else:
+            #state is currently a tensor containing 5 tensors, one for each image
+            val = val.max(1)[1].view(1, 1)
+        '''
+        policy_net.train()
+
+        return val
+    else:
+        return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
+
+
+episode_durations = []
+
+
+
+
+
+def optimize_model():
+
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+    # detailed explanation). This converts batch-array of Transitions
+    # to Transition of batch-arrays.
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.stack([s for s in batch.next_state
+                                                if s is not None])
+
+
+    state_batch = torch.stack(batch.state,dim=0)
+    action_batch = torch.stack(batch.action,dim=0).flatten(1)
+    reward_batch = torch.stack(batch.reward,dim=0)
+    '''
+    state_batch = batch.state
+    action_batch = batch.action
+    reward_batch = batch.reward
+    '''
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+
+
+    # Compute the expected Q values
+    # changed from the below to get * and + operations to behave element-wise
+    #expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+    next_state_values = next_state_values.squeeze()*GAMMA
+    reward_batch = reward_batch.squeeze()
+
+    expected_state_action_values = torch.add(next_state_values,reward_batch).unsqueeze(1)
+
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values)
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+
+
+episode_rewards = []
 
 print("##### Starting training ######")
 for i_episode in range(NUM_EPISODES):
 
     print("Episode: ", i_episode)
-    accuracy_sum = 0
+    reward_sum = 0
 
 
     # Initialize the environment and state
@@ -275,28 +434,15 @@ for i_episode in range(NUM_EPISODES):
     state = env.state
     next_state = env.state
 
-    print(policy_net(state))
-
     print("Training for ", STEPS_PER_EP, " steps.")
-    #loop until the environment indicates the end of an episode
     for t in tqdm(count()):
-
-
-        pred = policy_net(state)
-
-        if(env.current_label[-1] < 0): label = torch.tensor([0]).to(device)
-        else: label = torch.tensor([1]).to(device)
-
-        #print(pred.max(1)[1].view(1, 1).item())
-        #print(label.item())
-        if(pred.max(1)[1].view(1, 1).item() == label.item()):
-            accuracy_sum += 1
-
-        loss = criterion(pred,label)
-
         # Select and perform an action
-        done = env.step()
+        action = select_action(state)
+        reward, done = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
 
+        #print(reward)
+        reward_sum += reward.item()
 
         # Observe new state
         if not done:
@@ -304,56 +450,54 @@ for i_episode in range(NUM_EPISODES):
         else:
             next_state = None
 
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
 
         # Move to the next state
         state = next_state
 
         # Perform one step of the optimization (on the policy network)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        #print('Step: ', t)
+        optimize_model()
         if done:
+            episode_durations.append(t + 1)
             break
     # Update the target network, copying all weights and biases in DQN
     if i_episode % TARGET_UPDATE == 0:
         target_net.load_state_dict(policy_net.state_dict())
 
-    #compute and store average accuracy
-    episode_accuracy.append((accuracy_sum/STEPS_PER_EP))
+    episode_rewards.append(reward_sum)
 
 print('Complete')
 
 # Plot and save loss
 X_axis = list(range(NUM_EPISODES))
 cur_plot = plt.figure()
-plt.plot(X_axis, episode_accuracy, label='Training Accuracy')
+plt.plot(X_axis, episode_rewards, label='Training Reward')
 
 #cur_plot.suptitle(str(model_name) + ' Loss: lr=' + str(lr))
 plt.xlabel('Episode')
-plt.ylabel('Accuracy')
+plt.ylabel('Reward')
 plt.legend()
-plt.title("Average Training Accuracy: "+ str(sum(episode_accuracy)/len(episode_accuracy)))
+plt.title("Average Training Reward: "+ str(sum(episode_rewards)/len(episode_rewards)))
 # plt.show()
-plt.savefig('./Training_base_cnn_lstm.png')
+plt.savefig('./Deep_Q_Results.png')
 
 # clear plot for next iteration
 plt.clf()
-
-
 
 '''##################################################################'''
 '''#################### Now evaluate the model ######################'''
 '''##################################################################'''
 
-episode_accuracy = []
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
+print("##### Starting validation ######")
 
-print("##### Starting Validation ######")
+episode_rewards = []
+
 for i_episode in range(VALIDATION_EPISODES):
 
     print("Episode: ", i_episode)
-    accuracy_sum = 0
+    reward_sum = 0
 
 
     # Initialize the environment and state
@@ -365,29 +509,23 @@ for i_episode in range(VALIDATION_EPISODES):
     print("Episode starting from t= ",start_t," to t= ",(start_t+STEPS_PER_EP))
     env.set_start_offset(start_t)
 
+
+    # Adjust the max_steps to match the validation indexing
+    # We subtract 2 because we depend on future data for a label
+    env.max_steps = env.total_steps - 2
+
+
     state = env.state
     next_state = env.state
 
-    print(policy_net(state))
-
-    print("Training for ", STEPS_PER_EP, " steps.")
-    #loop until the environment indicates the end of an episode
     for t in tqdm(count()):
-
-
-        pred = target_net(state)
-
-        if(env.current_label[-1] < 0): label = torch.tensor([0]).to(device)
-        else: label = torch.tensor([1]).to(device)
-
-        #print(pred.max(1)[1].view(1, 1).item())
-        #print(label.item())
-        if(pred.max(1)[1].view(1, 1).item() == label.item()):
-            accuracy_sum += 1
-
         # Select and perform an action
-        done = env.step()
+        action = select_action(state)
+        reward, done = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
 
+        #print(reward)
+        reward_sum += reward.item()
 
         # Observe new state
         if not done:
@@ -395,32 +533,118 @@ for i_episode in range(VALIDATION_EPISODES):
         else:
             next_state = None
 
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
 
         # Move to the next state
         state = next_state
 
-
+        #no need to optimise the model in validation, so that portion has been removed
         if done:
+            episode_durations.append(t + 1)
             break
-
-
-    #compute and store average accuracy
-    episode_accuracy.append((accuracy_sum/STEPS_PER_EP))
+    '''
+    # Update the target network, copying all weights and biases in DQN
+    if i_episode % TARGET_UPDATE == 0:
+        target_net.load_state_dict(policy_net.state_dict())
+    '''
+    episode_rewards.append(reward_sum)
 
 print('Complete')
 
 # Plot and save loss
-X_axis = list(range(NUM_EPISODES))
+X_axis = list(range(VALIDATION_EPISODES))
 cur_plot = plt.figure()
-plt.plot(X_axis, episode_accuracy, label='Validation Accuracy')
+plt.plot(X_axis, episode_rewards, label='Validation Reward')
 
 #cur_plot.suptitle(str(model_name) + ' Loss: lr=' + str(lr))
 plt.xlabel('Episode')
-plt.ylabel('Accuracy')
+plt.ylabel('Reward')
 plt.legend()
-plt.title("Average Validation Accuracy: "+ str(sum(episode_accuracy)/len(episode_accuracy)))
+plt.title("Average Validation Reward: "+ str(sum(episode_rewards)/len(episode_rewards)))
 # plt.show()
-plt.savefig('./Validation_base_cnn_lstm.png')
+plt.savefig('./Deep_Q_Results_VAL.png')
+
+# clear plot for next iteration
+plt.clf()
+
+
+'''##################################################################'''
+'''####### Evaluate model on the entire evaluation timeline #########'''
+'''##################################################################'''
+print("##### Validating on all reserved data at once ######")
+episode_rewards = []
+
+for i_episode in range(1):
+
+    print("Episode: ", i_episode)
+    reward_sum = 0
+
+
+    # Initialize the environment and state
+    env = environment(DATA_ROOT,validation_split=VALIDATION_SPLIT,
+                      steps_per_ep=(env.total_steps - (env.validation_index + SEQUENCE_LENGTH)),
+                      sequence_length=SEQUENCE_LENGTH)
+
+    #Start every episode at a random point in time
+    start_t = env.validation_index
+    print("Episode starting from t= ",start_t," to final t= ",(env.total_steps))
+    env.set_start_offset(start_t)
+
+
+    # Adjust the max_steps to match the validation indexing
+    # We subtract 2 because we depend on future data for a label
+    env.max_steps = env.total_steps - 2
+
+    state = env.state
+    next_state = env.state
+
+    for t in tqdm(count()):
+        # Select and perform an action
+        action = select_action(state)
+
+        reward, done = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+
+        #print(reward)
+        reward_sum += reward.item()
+
+        # Observe new state
+        if not done:
+            next_state = env.state
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        #no need to optimise the model in validation, so that portion has been removed
+        if done:
+            episode_durations.append(t + 1)
+            break
+    # Update the target network, copying all weights and biases in DQN
+    if i_episode % TARGET_UPDATE == 0:
+        target_net.load_state_dict(policy_net.state_dict())
+
+    episode_rewards.append(reward_sum)
+
+print('Complete')
+
+# Plot and save loss
+X_axis = list(range(1))
+cur_plot = plt.figure()
+plt.plot(X_axis, episode_rewards, label='Validation Reward')
+
+#cur_plot.suptitle(str(model_name) + ' Loss: lr=' + str(lr))
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.legend()
+plt.title("Average Validation Reward: "+ str(sum(episode_rewards)/len(episode_rewards)))
+# plt.show()
+plt.savefig('./Deep_Q_Results_VAL_entire_timeline.png')
 
 # clear plot for next iteration
 plt.clf()
